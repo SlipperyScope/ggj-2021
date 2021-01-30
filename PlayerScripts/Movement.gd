2d3d4b09f5b49db17095a0dd39a4922c32c44661
ADKauth <adamkauth@gmail.com>
2021-01-30 12:41:21 -0800

    Setup item pickup

--- PlayerScripts/Movement.gd
+++ PlayerScripts/Movement.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 export (int) var speed = 200
 onready var DialogueBox = $DialogueBox
 onready var Steve: AnimatedSprite = get_node("Steve")
+onready var _Inventory = get_node("Inventory")
 
 var velocity = Vector2()
-var Dialogue
+#var Dialogue
+var Interactor
 var DialogueBoxWorking = false
 
 func _ready():
@@ -34,22 +36,34 @@ func _physics_process(_delta):
 		else: Steve.animation = "idle"
 
 func _process(_delta):
-	if (!DialogueBoxWorking):
-		if Input.is_action_pressed('ui_accept') && Dialogue != null: _start_dialogue()
+	if (!DialogueBoxWorking && Interactor):
+		if Input.is_action_pressed('ui_accept'):
+			if Interactor.has_node("Dialogue"): _start_dialogue()
+			elif Interactor.has_node("Item"): _add_item()
+			#if dialogue else if item->Inventory.AddItem() else if....
 
-func _on_Area2D_area_entered(area):
-	if area.has_node("../Dialogue"): 
-		Dialogue = area.get_node("../Dialogue")
+func _on_Area2D_area_entered(_area):
+	Interactor = _area.get_node("../")
+	if Interactor.has_node("Notification"):
+		Interactor.get_node("Notification").show()#do a little dance
+
+	if Interactor.has_node("Dialogue"): 
+		var Dialogue = Interactor.get_node("Dialogue")
 		if Dialogue.ForceStart: _start_dialogue()
-		else: area.get_node("../Notification").show()
 
-func _on_Area2D_area_exited(area):
-	if area.has_node("../Dialogue"): Dialogue = null
-	if area.has_node("../Notification"): area.get_node("../Notification").hide()
+func _on_Area2D_area_exited(_area):
+	if Interactor != null:
+		if Interactor.has_node("Notification"): Interactor.get_node("Notification").hide() #stop a little dance
+		Interactor = null
 
 func _start_dialogue():
-	DialogueBox.StartDialogue(Dialogue)
+	DialogueBox.StartDialogue(Interactor.get_node("Dialogue"))
 	DialogueBoxWorking = true
 
 func _print_finished():
 	DialogueBoxWorking = false
+
+func _add_item():
+	_Inventory.AddItem(Interactor.get_node("Item"))
+	Interactor.queue_free()
+	#remove Interactor
