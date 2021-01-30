e91e0cf64bf604186153ccf64262968221e0e0ab
ADKauth <adamkauth@gmail.com>
2021-01-29 21:38:59 -0800

    Player and npc created. Dialogue triggers attached

--- PlayerScripts/Movement.gd
+++ PlayerScripts/Movement.gd
@@ -1,8 +1,14 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
+onready var DialogueBox = $DialogueBox
 
 var velocity = Vector2()
+var Dialogue
+var DialogueBoxWorking = false
+
+func _ready():
+	DialogueBox.connect("DialogueFinished", self, "_print_finished")
 
 func get_input():
 	velocity = Vector2()
@@ -17,5 +23,27 @@ func get_input():
 	velocity = velocity.normalized() * speed
 
 func _physics_process(_delta):
-	get_input()
-	velocity = move_and_slide(velocity)
+	if (!DialogueBoxWorking):
+		get_input()
+		velocity = move_and_slide(velocity)
+
+func _process(_delta):
+	if (!DialogueBoxWorking):
+		if Input.is_action_pressed('ui_accept') && Dialogue != null: _start_dialogue()
+
+func _on_Area2D_area_entered(area):
+	if area.has_node("../Dialogue"): 
+		Dialogue = area.get_node("../Dialogue")
+		if Dialogue.ForceStart: _start_dialogue()
+		else: area.get_node("../Notification").show()
+
+func _on_Area2D_area_exited(area):
+	if area.has_node("../Dialogue"): Dialogue = null
+	if area.has_node("../Notification"): area.get_node("../Notification").hide()
+
+func _start_dialogue():
+	DialogueBox.StartDialogue(Dialogue)
+	DialogueBoxWorking = true
+
+func _print_finished():
+	DialogueBoxWorking = false
