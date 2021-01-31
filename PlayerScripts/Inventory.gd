85db16f349e8a35fbe1300c078455541c647f190
Tilde <pxn.tilde@protonmail.com>
2021-01-30 16:46:41 -0800

    embettered Inventory

--- PlayerScripts/Inventory.gd
+++ PlayerScripts/Inventory.gd
@@ -12,14 +12,16 @@ func _ready():
 func _DeferReady():
 	for child in get_children():
 		if child is Item:
-			AddItem(child)
+			AddItem(child, false)
 
-func AddItem(item):
+func AddItem(item, autoAddChild := true):
 	Items.append(item)
 	emit_signal("InventoryItemAdded", item)
 	emit_signal("InventoryUpdated", Items)
+	if autoAddChild:
+		add_child(item)
 
-func RemoveItem(item) -> bool:
+func RemoveItem(item, autoRemoveChild := true) -> bool:
 	var index = Items.find(item)
 	if index == -1:
 		return false
@@ -28,7 +30,24 @@ func RemoveItem(item) -> bool:
 	Items.remove(index)
 	emit_signal("InventoryItemRemoved", removedItem)
 	emit_signal("InventoryUpdated")
+
+	if autoRemoveChild:
+		remove_child(item)
 	return true
 
 func HasItem(item):
-	return Items.find(item) != -1
+	for i in Items:
+		if i.ItemType == item || i == item:
+			return true
+	
+	return false
+
+func GetItem(type) -> Item:
+	for item in Items:
+		if item.ItemType == type:
+			return item
+	return null
+
+func Transfer(otherInventory, item):
+	otherInventory.RemoveItem(item)
+	AddItem(item)
