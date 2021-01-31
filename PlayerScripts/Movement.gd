7f99871393967d192f1b82686464fdb421dad4d1
ADKauth <adamkauth@gmail.com>
2021-01-30 16:50:01 -0800

    Place items where they go

--- PlayerScripts/Movement.gd
+++ PlayerScripts/Movement.gd
@@ -39,7 +39,8 @@ func _process(_delta):
 	if (!DialogueBoxWorking && Interactor):
 		if Input.is_action_pressed('ui_accept'):
 			if Interactor.has_node("Dialogue"): _start_dialogue()
-			elif Interactor.has_node("Inventory"): _add_items()
+			elif Interactor.has_node("TakeMe"): _add_items()
+			elif Interactor.has_node("Inventory"): _place_item()
 
 func _on_Area2D_area_entered(_area):
 	Interactor = _area.get_node("../")
@@ -63,9 +64,25 @@ func _print_finished():
 	DialogueBoxWorking = false
 
 func _add_items():
-	for item in Interactor.get_node("Inventory").get_children():
+	for item in Interactor.get_node("TakeMe").get_children():
 		if item is Item:
-			_Inventory.AddItem(item)
+			_Inventory.AddItem(item, false)
 
-	Interactor.get_node("Inventory").queue_free()
+	Interactor.get_node("TakeMe").queue_free()
 	Interactor.get_node("Notification").queue_free()
+
+func _place_item():
+	var destInventory = Interactor.get_node("Inventory")
+	if !destInventory.SpecificItem: return
+
+	var requiredType = destInventory.RequiredItemType
+	var _placed = false
+
+	for item in _Inventory.Items:
+		if item.ItemType == requiredType:
+			destInventory.AddItem(item, false)
+			_Inventory.RemoveItem(item, false)
+			destInventory.RequiredItemType = null
+			_placed = true
+	
+	#do something with placed
